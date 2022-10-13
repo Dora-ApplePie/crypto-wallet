d8303349f3c62556f5425636189fac9e86dbc380
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-10-13 13:51:03 +0300

    create routes

---
+++ src/main/Routes/Routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Navigate, Route, Routes} from "react-router-dom";
+import CryptoPage from "../pages/CryptoPage/CryptoPage";
+import Page404 from "../pages/Page404/Page404";
+import MainPage from "../pages/MainPage/MainPage";
+
+
+export const PATH = {
+    MAIN_PAGE: '/homepage',
+    CRYPTO_PAGE: '/crypto:id'
+}
+
+const Pages: React.FC = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<Navigate to={PATH.MAIN_PAGE}/>}/>
+            <Route path={PATH.MAIN_PAGE} element={<MainPage/>}/>
+            <Route path={PATH.CRYPTO_PAGE} element={<CryptoPage/>}/>
+            <Route path={'/*'} element={<Page404/>}/>
+        </Routes>
+    )
+}
+
+export default Pages
