1cdf2d1ad4dc524537a419b0f3f06bc0728b5490
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-10-26 21:46:47 +0300

    add  reducer for crypto page

---
+++ src/main/pages/CryptoPage/cryptoPageReducer.ts
@@ -0,0 +1,39 @@
+import {AssetsResponseType, InitialCoinsStateType, InitialCurrentCoinStateType} from "../../../types/Crypto";
+import {AppThunk} from "../../../app/store";
+import {Dispatch} from "redux";
+import {cryptoApi} from "../../../api/cryptoApi";
+
+const initialState: InitialCurrentCoinStateType = {
+    currentCoin: null,
+    isLoading: true,
+    error: null,
+}
+
+export const cryptoPageReducer = (state: InitialCurrentCoinStateType = initialState, action: ActionsType): InitialCurrentCoinStateType => {
+    switch (action.type) {
+        case 'SET-COIN':
+            return {...state, currentCoin: action.currentCoin}
+        default:
+            return {...state}
+    }
+}
+
+export const setCoinAC = (currentCoin: AssetsResponseType) => ({type: 'SET-COIN', currentCoin} as const)
+
+
+export const fetchCurrentCoinTC = (id: string | undefined): AppThunk => (dispatch: Dispatch) => {
+    cryptoApi.getCoinById(id)
+        .then(res => {
+            dispatch(setCoinAC(res.data.data));
+        })
+    //     .catch(res => {
+    //         dispatch(setErrorAC('Some error occurred'))
+    //     })
+    //     .finally(() => {
+    //         dispatch(setIsLoadingAC(false));
+    //     })
+}
+
+export type SetCoinActionType = ReturnType<typeof setCoinAC>
+
+type ActionsType = SetCoinActionType
