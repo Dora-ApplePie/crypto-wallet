e90e2ee791698bec7474949cc26fa5133bf91c30
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-10-28 00:01:08 +0300

    some refactoring

--- src/main/pages/CryptoPage/CryptoInfo/CryptoInfo.tsx
+++ src/main/pages/CryptoPage/CryptoInfo/CryptoInfo.tsx
@@ -5,7 +5,7 @@ import style from "../CryptoPage.module.scss";
 import {useNavigate} from "react-router-dom";
 import {ICryptoPageInfo} from "../../../../types/Crypto";
 
-const CryptoInfo: React.FC<ICryptoPageInfo> = ({ currentCoin }: ICryptoPageInfo) => {
+const CryptoInfo: React.FC<ICryptoPageInfo> = ({currentCoin}: ICryptoPageInfo) => {
     const navigate = useNavigate()
 
     return <div>
@@ -16,12 +16,9 @@ const CryptoInfo: React.FC<ICryptoPageInfo> = ({ currentCoin }: ICryptoPageInfo)
         <div className={currentCoin.changePercent24Hr[0] === "-" ? style.decrease : style.increase}>
             {`${convertNumber(currentCoin.changePercent24Hr)}%`}
         </div>
-
-
         <div>Market cap: {convertNumber(currentCoin.marketCapUsd)}</div>
         <div>Volume: {convertNumber(currentCoin.volumeUsd24Hr)}</div>
         <div>Supply: {convertNumber(currentCoin.maxSupply)}</div>
-
         <CustomButton success>Add to briefcase</CustomButton>
     </div>
 }
