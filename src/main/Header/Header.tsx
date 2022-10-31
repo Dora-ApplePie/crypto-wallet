410165da631ac3d48701d4b3f7a54ccbfe1fae38
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-10-31 21:21:04 +0300

    show at the header briefcase btn current price of amount crypto coins

--- src/main/Header/Header.tsx
+++ src/main/Header/Header.tsx
@@ -1,22 +1,33 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import style from "./Header.module.scss"
 import CustomButton from "../common/CustomButton/CustomButton";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBriefcase} from "@fortawesome/free-solid-svg-icons/faBriefcase";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
-import {fetchAllCoinsTC} from "../pages/MainPage/mainPageReducer";
+import {fetchAllCoinsTC} from "../components/MainPage/mainPageReducer";
 import convertNumber from "../utils/convertNumbers";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {roundTo} from "../utils/roundTo";
+import {getCurrentCryptoPriceListTC} from "../components/Briefcase/AddToBriefcaseModal/addToBriefcaseModalReducer";
 
 
 const Header: React.FC = () => {
 
     const dispatch = useAppDispatch();
+    const { pathname } = useLocation();
+    const [isOpen, setIsOpen] = useState(false)
     const coins = useAppSelector((state) => state.main.allCoins)
+    const initialSum = useAppSelector((state) => state.briefcase.initialSum)
+    const currentSum = useAppSelector((state) => state.briefcase.currentSum)
+    console.log(currentSum)
 
     useEffect(() => {
         dispatch(fetchAllCoinsTC())
     }, [])
+
+    useEffect(() => {
+        dispatch(getCurrentCryptoPriceListTC())
+    }, [pathname])
 
     return (
         <div className={style.header}>
@@ -25,13 +36,18 @@ const Header: React.FC = () => {
                     {coins && coins.slice(0, 3).map(coin =>
                         <Link to={`main/assets/${coin.id}`} key={coin.id} className={style.currencyInfo}>
                             {coin.symbol}
-                            <span>{convertNumber(coin.priceUsd)}</span>
+                            <span>{roundTo(coin.priceUsd)}</span>
                         </Link>
                     )}
                 </div>
-                <CustomButton dark className={style.caseButton}>
+                <CustomButton dark className={style.caseButton} onClick={() => setIsOpen(true)}>
                     <FontAwesomeIcon className={style.caseFontIcon} icon={faBriefcase}/>
-                    Briefcase Value
+                    {
+                        initialSum > 0
+                            ?
+                            `${roundTo(`${currentSum}`)} USD (${(currentSum - initialSum) > 0 ? '+' : ''}${roundTo(`${(currentSum - initialSum) / initialSum * 100}`)}%)`
+                            : '0 USD'
+                    }
                 </CustomButton>
             </div>
         </div>
